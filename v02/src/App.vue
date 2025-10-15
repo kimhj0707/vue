6b005c74d2341156eb034eb73723e649df51dc92
김현중 <ayo0707@naver.com>
2025-10-15 16:45:57 +0900

    clas 바인딩

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -2,24 +2,26 @@
 export default {
   data() {
     return {
-      site_name: "Daum",
-      url: "https://daum.net",
-      src: "https://i.namu.wiki/i/xxyTLDbYnN4uGEFNR-VpNVMa-iK5c5-zvzd4VOoBOuMBfzhqx1K8mcToaQ7K17D7OoqypoJolu3uAKURgooBOQ.webp",
+      bgClass: "bg",
+      erClass: "text-danger",
+      isActive: true,
     };
-  },
-  methods: {
-    clickAr: function () {
-      this.site_name = "Naver";
-      this.url = "https://naver.com";
-    },
   },
 };
 </script>
 
 <template>
-  <a :href="url">{{ site_name }}</a>
-  <img :src="src" />
-  <button @click="clickAr">버튼</button>
+  <div class="border" :class="[isActive ? bgClass : '', erClass]">Warning</div>
 </template>
 
-<style></style>
+<style>
+.border {
+  border: 2px solid red;
+}
+.bg {
+  background-color: gray;
+}
+.text-danger {
+  color: red;
+}
+</style>
