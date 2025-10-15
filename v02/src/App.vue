d361dd22b83a086ad939e6782ac684ef3abe852a
김현중 <ayo0707@naver.com>
2025-10-15 12:01:35 +0900

    first commit

---
+++ v02/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+      channel: "KHJ Vue.js",
+      subscribe: 2677,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>채널명 : {{ channel }}</h1>
+  <h2>구독자수 : {{ subscribe }}</h2>
+  <p>{{ `오늘의 행운 숫자:` + Math.ceil(Math.random() * 45) }}</p>
+</template>
+
+<style></style>
