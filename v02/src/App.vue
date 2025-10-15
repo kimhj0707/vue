8fbc08321844717a189e4a2f437a97d3bab8f74d
김현중 <ayo0707@naver.com>
2025-10-15 14:05:17 +0900

    v-for

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -2,17 +2,16 @@
 export default {
   data() {
     return {
-      channel: "KHJ Vue.js",
-      subscribe: 2677,
+      sports: ["Baseball", "Football", "Volleyball", "Swimming"],
     };
   },
 };
 </script>
 
 <template>
-  <h1>채널명 : {{ channel }}</h1>
-  <h2>구독자수 : {{ subscribe }}</h2>
-  <p>{{ `오늘의 행운 숫자:` + Math.ceil(Math.random() * 45) }}</p>
+  <ul>
+    <li v-for="(sport, index) in sports">{{ index + 1 }} {{ sport }}</li>
+  </ul>
 </template>
 
 <style></style>
