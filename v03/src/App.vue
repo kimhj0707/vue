dc42d8869aeb9e36b4652203b1911ce44273edba
김현중 <ayo0707@naver.com>
2025-10-16 18:01:19 +0900

    값을 전달할 때 사용하는 props

---
+++ v03/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>나는 부모</h1>
+  <button @click="ageInc">age ++</button>
+  <ChildCom v-bind:name="name" v-bind:age="age" />
+</template>
+
+<script>
+import ChildCom from "./components/ChildComponent.vue";
+export default {
+  data() {
+    return {
+      name: "Khj",
+      age: 35,
+    };
+  },
+  components: {
+    ChildCom,
+  },
+  methods: {
+    ageInc() {
+      this.age++;
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ v03/src/components/ChildComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Child Component</h1>
+  <p>Name : {{ name }}</p>
+  <p>Age : {{ age }}</p>
+</template>
+
+<script>
+export default {
+  props: ["name", "age"],
+  created() {
+    console.log(this.name);
+  },
+  methods: {
+    propsUpdater() {
+      this.age = 30;
+    },
+  },
+};
+</script>
