5b6746020b7423be0059573dd597709eaa79cbfe
김현중 <ayo0707@naver.com>
2025-10-16 17:28:17 +0900

    $refs, ref document.getElementById 랑 비슷

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,52 +1,17 @@
 <template>
-  <h1>App.Vue</h1>
-  <h1>Count : {{ cnt }}</h1>
-  <GlobalChild v-if="cnt == 2" />
-  <button @click="counter">Count + 1</button>
-  <button @click="discounter">Count - 1</button>
+  <div>
+    <input type="text" ref="myInput" />
+    <button @click="logInput">Log</button>
+  </div>
 </template>
 
 <script>
-import GlobalChild from "./components/GlobalChild.vue";
-
 export default {
-  data() {
-    return {
-      cnt: 1,
-    };
-  },
-  beforeCreate() {
-    console.log("beforeCreate");
-  },
-  created() {
-    console.log("created");
-  },
-  beforeMount() {
-    console.log("beforeMount");
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  beforeUpdate() {
-    console.log("beforeUpdate");
-  },
-  updated() {
-    console.log("updated");
-  },
-  deforeUnmount() {
-    console.log("deforeUnmount");
-  },
-  unmounted() {
-    console.log("unmounted");
-  },
   methods: {
-    counter() {
-      this.cnt++;
-    },
-    discounter() {
-      this.cnt--;
+    logInput() {
+      console.log(this.$refs.myInput.value);
+      // console.log(document.getElementById("myInput").value);
     },
   },
 };
 </script>
-<style lang="scss" scoped></style>
