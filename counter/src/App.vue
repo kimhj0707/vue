8868082de73d085670c5e445a0930453de90d3f8
김현중 <ayo0707@naver.com>
2025-10-17 16:07:28 +0900

    Counter 폴더 compositionAPI

---
+++ counter/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>{{ cnt }}</p>
+  <p>홀짝 판별 : {{ oddEven }}</p>
+  <button @click="counter">클릭</button>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const cnt = ref(0);
+    const oddEven = computed(() => (cnt.value % 2 ? "홀수" : "짝수"));
+    const counter = () => cnt.value++;
+
+    return {
+      cnt,
+      oddEven,
+      counter,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
