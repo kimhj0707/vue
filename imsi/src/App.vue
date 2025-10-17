819cbb9db8492c9a57365bc382a6938cbb522187
김현중 <ayo0707@naver.com>
2025-10-17 16:51:03 +0900

    compositionAPI computed

--- imsi/src/App.vue
+++ imsi/src/App.vue
@@ -1,43 +1,18 @@
 <template>
-  <h1 :class="[{ red: isRed, black: !isRed }]">나를 빨갛게 만들어 보세요.</h1>
-
-  <button @click="makeRed">빨강</button>
-  <button @click="makeBlack">검정</button>
-  <button @click="toggleColor">토글</button>
+  <p>cnt : {{ cnt }}</p>
+  <p>홀짝 판별 : {{ oddEven }}</p>
+  <button @click="counter">클릭</button>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+const cnt = ref(10);
 
-// true면 빨강, false면 검정
-const isRed = ref(false);
+const oddEven = computed(() => (cnt.value % 2 ? "홀수" : "짝수"));
 
-// 기존 good1과 동일: 빨갛게
-const makeRed = () => {
-  isRed.value = true;
+const counter = () => {
+  cnt.value++;
 };
-
-// 요청하신 "function" 버전: 다시 검게(원래색) 만들기
-function makeBlack() {
-  isRed.value = false;
-}
-
-// 참고용: 토글도 추가
-function toggleColor() {
-  isRed.value = !isRed.value;
-}
 </script>
 
-<style scoped>
-.red {
-  color: red;
-}
-.black {
-  color: #111; /* 검정(원래색), 필요시 다른 색상으로 변경 가능 */
-}
-
-/* 버튼 보이기 좋게(참고용, 없어도 됨) */
-button {
-  margin-right: 6px;
-}
-</style>
+<style lang="scss" scoped></style>
