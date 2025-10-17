21fb5c83ac1fed1247fbc8a2cb16eab1e2877ba0
김현중 <ayo0707@naver.com>
2025-10-17 16:46:28 +0900

    동적으로 클래스 적용하기

--- imsi/src/App.vue
+++ imsi/src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <h1 ref="a" id="b">안녕하세요. 김현중 vue 연습입니다.</h1>
-  <button @click="good">봐튼</button>
+  <h1 :class="[{ red: isRed, black: !isRed }]">나를 빨갛게 만들어 보세요.</h1>
+
+  <button @click="makeRed">빨강</button>
+  <button @click="makeBlack">검정</button>
+  <button @click="toggleColor">토글</button>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-const a = ref(null);
+// true면 빨강, false면 검정
+const isRed = ref(false);
 
-const good = () => {
-  // alert(a.value.innerText);
-  alert(document.getElementById("b").innerText);
+// 기존 good1과 동일: 빨갛게
+const makeRed = () => {
+  isRed.value = true;
 };
+
+// 요청하신 "function" 버전: 다시 검게(원래색) 만들기
+function makeBlack() {
+  isRed.value = false;
+}
+
+// 참고용: 토글도 추가
+function toggleColor() {
+  isRed.value = !isRed.value;
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.red {
+  color: red;
+}
+.black {
+  color: #111; /* 검정(원래색), 필요시 다른 색상으로 변경 가능 */
+}
+
+/* 버튼 보이기 좋게(참고용, 없어도 됨) */
+button {
+  margin-right: 6px;
+}
+</style>
