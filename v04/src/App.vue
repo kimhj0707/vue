4e4b84f76dd6985fa9c79fa30b798006ce7d76ae
김현중 <ayo0707@naver.com>
2025-10-17 10:43:46 +0900

    부모 Components 에게 요청하는 emit

---
+++ v04/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>나는 부모 Component({{ c }})</h1>
+  <LocalCom @create-post="callFunction" />
+</template>
+
+<script>
+import LocalCom from "@/components/LocalChild.vue";
+export default {
+  data() {
+    return {
+      c: 0,
+    };
+  },
+  components: {
+    LocalCom,
+  },
+  methods: {
+    callFunction(var1) {
+      console.log("자식 컴포넌트에서 보낸 이벤트를 수신했어요." + var1);
+      this.c = var1;
+    },
+  },
+};
+</script>
+
+<style></style>
+
+<!-- aaa bbb ccc -->
+<!-- aaa-bbb-ccc 케밥 케이스 -->
+<!-- aaa_bbb_ccc 뱀 케이스 -->
+<!-- aaaBbbCcc 낙타 -->
+<!-- AaaBbbCcc 파스칼 -->
---
+++ v04/src/components/LocalChild.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    나는 Child component 입니다. ({{ c }})
+    <input type="text" ref="myInput" v-model="c" />
+    <button @click="emitToParent">부모 이벤트 호출</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      c: 5,
+    };
+  },
+  methods: {
+    emitToParent() {
+      this.$emit("createPost", this.c);
+    },
+  },
+};
+</script>
