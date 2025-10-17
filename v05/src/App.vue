f9fbb7f6d19abdb5269b8cd59bc94cbd941eba1e
김현중 <ayo0707@naver.com>
2025-10-17 11:13:33 +0900

    provide와 inject

---
+++ v05/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>안녕하세요. 할아버지에요.</h1>
+    <Child />
+  </div>
+</template>
+
+<script>
+import Child from "@/components/FirstChild.vue";
+export default {
+  data() {
+    return {
+      gift: "장난감",
+    };
+  },
+  components: {
+    Child,
+  },
+  provide() {
+    return {
+      gift: this.gift,
+    };
+  },
+};
+</script>
+
+<style>
+h1 {
+  color: red;
+}
+span {
+  margin-left: 20px;
+}
+</style>
---
+++ v05/src/components/SecondChild.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sub">
+    <h1>손자 컴포넌트</h1>
+    <p>할아버지로부터 받은 선물 : {{ gift }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ["gift"],
+};
+</script>
+
+<style scoped>
+.sub {
+  width: 250px;
+  height: 130px;
+  border: 1px solid blue;
+  margin: 10px;
+}
+</style>
