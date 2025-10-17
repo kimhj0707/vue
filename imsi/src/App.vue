52199a57f0771318b3ef967c5fb294dea678d207
김현중 <ayo0707@naver.com>
2025-10-17 16:29:23 +0900

    Ref 와 Reactive

---
+++ imsi/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 ref="a" id="b">안녕하세요. 김현중 vue 연습입니다.</h1>
+  <button @click="good">봐튼</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const a = ref(null);
+
+const good = () => {
+  // alert(a.value.innerText);
+  alert(document.getElementById("b").innerText);
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ imsi/src/App_options.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>{{ cnt }}</p>
+  <p>홀짝 판별 : {{ oddEven }}</p>
+  <button @click="counter">클릭</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cnt: 0,
+    };
+  },
+  computed: {
+    oddEven() {
+      return this.cnt % 2 ? "홀수" : "짝수";
+    },
+  },
+  methods: {
+    counter() {
+      this.cnt++;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
